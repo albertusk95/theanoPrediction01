261385c79c66c186f24b1b4b1fb4a19b396ba748
albertusk95 <albertusk15@gmail.com>
2017-05-28 16:56:00 +0700

    model architecture done

--- cnnkeras.py
+++ cnnkeras.py
@@ -26,4 +26,16 @@ X_test /= 255
 Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10)
 
-print Y_train[:3]
+# Build the model architecture
+model = Sequential()
+
+# Declare the CNN input layer
+model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(1,28,28)))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+ 
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
